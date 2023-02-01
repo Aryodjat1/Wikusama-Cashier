70e93e15c41ad563600897c79762ff5521c9db82
Aryodjat1 <aryodjati5@gmail.com>
2023-02-01 09:48:04 +0700

    first commit with login activity

---
+++ app/src/main/java/com/wikusama/kasir/login/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.wikusama.kasir.login.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
